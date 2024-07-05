6a5a46c5424e684d05b1b9bcb1935dc530a66c40
AgusCuculich <cuculichagustina@gmail.com>
2024-07-04 23:28:27 -0300

    Objeto observable - Código del servicio modificado para que ahora el carrito de compras sea un objeto observable. - Código html relacionado a las secciones en el app component (cart y products) movidas a sus respectivos componentes. - Estilos de las secciones mencionadas anteriormente movidas a las hojas de estilos de sus componentes correspondientes.

--- src/app/product-cart.service.ts
+++ src/app/product-cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product-showcase/Product';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +8,14 @@ import { Product } from './product-showcase/Product';
 export class ProductCartService {
   //Maneja la lógica del carrito.
 
-  shoppingCart: Product[] = []; //Arreglo vacío donde irán todos los productos que se añadan al carrito de compras.
+  private _shoppingCart: Product[] = [];
+  shoppingCart: BehaviorSubject<Product[]> = new BehaviorSubject(this._shoppingCart);
+  /**
+   * 1. Arreglo vacío donde irán todos los productos que se añadan al carrito de compras.
+   * 2. Encapsulamos el carrito de compras en un BehaviourSubject para poder suscribirnos a sus cambios.
+   * 3. El BehaviourSubject encapsula una variable privada. Se considera una buena práctica crear una 
+   * propiedad privada con el mismo nombre y que inicie con un guión bajo.
+   */
 
   constructor() { }
 
@@ -19,16 +27,18 @@ export class ProductCartService {
      * 
      * Product | undefined --> En TypeScript, esto significa que la variable item podría ser de tipo Product o undefined.
      */
-    let item: Product | undefined = this.shoppingCart.find((x) => x.id == product.id);
+    let item: Product | undefined = this._shoppingCart.find((x) => x.id == product.id);
 
     if (!item) {
-      this.shoppingCart.push({... product});  
+      this._shoppingCart.push({... product});
+      this.shoppingCart.next(this._shoppingCart);
     }
     /**
-     * El if anterior checkea que el item no este exista en el carrito de compras para así añadirlo (nos asegura que no se 
+     * 1. El if anterior checkea que el item no este exista en el carrito de compras para así añadirlo (nos asegura que no se 
      * repitan items iguales en el carrito).
+     * 2. El BehaviourSubject no detecta los cambios, con lo que hay que indicarle que emita (next) los mismos.
      */
 
-    console.log(this.shoppingCart);
+    //console.log(this._shoppingCart);
   }
 }
--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductCartService } from '../product-cart.service';
+import { Product } from '../product-showcase/Product';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -9,9 +10,17 @@ import { ProductCartService } from '../product-cart.service';
   styleUrl: './shopping-cart.component.scss'
 })
 export class ShoppingCartComponent implements OnInit {
+  /**
+   * Esta clase se encarga del carrito de compras. Utiliza el servicio "ProductCartService" para siempre tener la última versión 
+   * del arreglo del carrito de compras. Cada vez que hay un cambio en esta lista, el servicio avisa a la clase, y la clase 
+   * actualiza su propia lista de productos
+   */
+  
+  shoppingCart: Product[] | undefined;
 
-  constructor(private cart: ProductCartService) { }
-
-  ngOnInit(): void {
+  constructor(private cart: ProductCartService) {
+    cart.shoppingCart.subscribe((arrCompras) => this.shoppingCart = arrCompras);
   }
+
+  ngOnInit(): void { }
 }
