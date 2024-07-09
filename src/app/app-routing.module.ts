6310ea206007136917667a48f457ebaff36f8e9d
AgusCuculich <cuculichagustina@gmail.com>
2024-07-08 21:48:21 -0300

    Enrutamiento añadido - Se rutean los componentes. - Se crea el componente "distributor-access" que contendrá un form para loggearse (vacío por ahora). - Se crea un nuevo componente padre (main-content) de los componentes mostrados en inicio/home. - Estilos de main movidos a su nuevo componente correspondiente.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { DistributorAccessComponent } from './distributor-access/distributor-access.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {                                          
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },{
+    path: 'home',
+    component: MainContentComponent
+  },{
+    path: 'login',
+    component: DistributorAccessComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { ProductShowcaseComponent } from './product-showcase/product-showcase.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { provideHttpClient } from '@angular/common/http';
+import { MainContentComponent } from './main-content/main-content.component';
+import { DistributorAccessComponent } from './distributor-access/distributor-access.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContentComponent,
+    DistributorAccessComponent,
   ],
   imports: [
     BrowserModule,
